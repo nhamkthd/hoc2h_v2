8bb716cb2cc2384f2f625c41747b1c84cc5e787d
nhamkthd <nhamkthd92@gmail.com>
2017-07-02 23:05:49 +0700

    add store question route
    missed store question show

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group(['prefix' => 'questions'], function(){
 	Route::get('/','QuestionController@index')->name('questions');
 	Route::get('/question-create','QuestionController@create')->name('showQuestionCreateFrom');
 	Route::get('/question/{id}','QuestionController@showDetail');
+	Route::post('/store','QuestionController@store')->name('storeQuestion');
 	Route::group(['prefix' => 'api'], function(){
 		//this is group route api angular js
 		Route::post('/vote','QuestionController@vote');
