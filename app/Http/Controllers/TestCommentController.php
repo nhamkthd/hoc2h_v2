ede8355650cf736df4773cdc4642a0c7aeba79fc
phamtuananh1996 <phamtuananh760@gmail.com>
2017-08-31 00:15:56 +0700

    update

--- app/Http/Controllers/TestCommentController.php
+++ app/Http/Controllers/TestCommentController.php
@@ -19,7 +19,7 @@ class TestCommentController extends Controller
     }
     public function getCommentTest($test_id)
     {
-      $TestComment=TestComment::where('test_id',$test_id)->orderBy('id','DESC')->paginate(10);
+      $TestComment=TestComment::where('test_id',$test_id)->orderBy('id','DESC')->paginate(5);
        foreach ( $TestComment as $comment) {
             $comment->user;
             $this->setDateFomat($comment);
